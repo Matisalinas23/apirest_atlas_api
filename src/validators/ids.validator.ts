5988267cdf787a8622c19b31f36fcca86680b442
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-30 15:22:17 -0300

    refactor: improve validateId logic and add return type annotation

--- src/validators/ids.validator.ts
+++ src/validators/ids.validator.ts
@@ -1,16 +1,16 @@
 import { TypeOfError } from "../errors/TypeOfError";
 
-export const validateId = (id: number) => {
-    if (typeof id !== "number") {
-        throw new TypeOfError("Id must be a number");
+export const validateId = (id: number): number => {
+    if (Number.isNaN(id)) {
+        throw new TypeOfError("Id must be a valid number");
+    }
+
+    if (!Number.isInteger(id)) {
+        throw new TypeOfError("Id must be an integer");
     }
 
     if (id <= 0) {
         throw new TypeOfError("Id must be a positive number");
-    }
-
-    if (isNaN(id)) {
-        throw new TypeOfError("Id must be a valid number");
     }
 
     return id;
