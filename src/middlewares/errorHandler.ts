c8d30894477796885ab84472b7371158fe14752a
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-30 17:12:05 -0300

    refactor: prisma error management removed from the global error handler middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,17 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { Prisma } from "@/generated/prisma/client";
 import { CustomError } from "@/errors/CustomError";
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
-    const KnownRequestError = Prisma.PrismaClientKnownRequestError
-
     console.error(error);
-
-    if (error instanceof KnownRequestError && error.code === "P1001") {
-        return res.status(503).json({
-            message: "Unable to connect to the database"
-        })
-    }
 
     if (error instanceof CustomError) {
         return res.status(error.statusCode).json({
