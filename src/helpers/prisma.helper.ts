44e6303baf488cd8723382f755fb0cd7f5373561
Matisalinas23 <maatisalinas18@gmail.com>
2026-07-03 18:32:33 -0300

    refactor: now the prisma helper catch the original prisma message from the driver adapter error property

--- src/helpers/prisma.helper.ts
+++ src/helpers/prisma.helper.ts
@@ -3,6 +3,7 @@ import { ServiceUnavailableError } from "../errors/ServiceUnavailableError"
 import { NotFoundError } from "../errors/NotFoundError"
 import { BadRequestError } from "../errors/BadRequestError"
 import { ConflictError } from "../errors/ConflictError"
+import { ColumnNotFound } from "../errors/ColumnNotFound"
 
 export const handlePrismaError = (error: unknown, resource: string) => {
     const KnownRequestError = Prisma.PrismaClientKnownRequestError
@@ -10,15 +11,25 @@ export const handlePrismaError = (error: unknown, resource: string) => {
     if (!(error instanceof KnownRequestError))
         throw error
 
+    const originalMessage = (
+        error.meta?.driverAdapterError as {
+            cause?: {
+                originalMessage?: string;
+            };
+        } | undefined
+    )?.cause?.originalMessage;
+
     switch (error.code) {
         case "P1001":
             throw new ServiceUnavailableError("Unable to connect to the database");
         case "P2002":
-            throw new ConflictError(`${resource} with this name already exists`);
+            throw new ConflictError(`${originalMessage ?? `${resource} with this name already exists`}`);
         case "P2003":
-            throw new BadRequestError(`${resource} sent with invalid input`);
+            throw new BadRequestError(originalMessage ?? `${resource} sent with invalid input`);
+        case "P2022":
+            throw new ColumnNotFound(originalMessage ?? `The column ${resource} doesn't exist`);
         case "P2025":
-            throw new NotFoundError(`${resource} with this id doesn't exist`);
+            throw new NotFoundError(originalMessage ?? `${resource} with this id doesn't exist`);
         default:
             throw error
     }
