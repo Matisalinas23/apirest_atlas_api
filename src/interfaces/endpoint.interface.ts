c85350a4f997d753d608cb4921736d980b2d7a89
Matisalinas23 <maatisalinas18@gmail.com>
2026-07-03 18:33:12 -0300

    feat: new endpoint dto interface and http method interface added

--- src/interfaces/endpoint.interface.ts
+++ src/interfaces/endpoint.interface.ts
@@ -2,6 +2,20 @@ import { HeaderResponse } from "./header.interface"
 import { ParameterResponse } from "./parameter.interface"
 import { ResponseResponse } from "./response.interace"
 
+type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD"
+
+export interface EndpointDto {
+    name: string
+    description: string
+    notes: string
+    path: string
+    requestBody: string
+    moduleId: number
+    tags: string[]
+
+    method: HttpMethod
+}
+
 export interface EndpointResponse {
     id: number
     name: string
